d0012c46ab40f407d96dc7eb81cc2d3d827b07e3
Mursil Shadruddin Afkar <afkarshad@gmail.com>
2017-05-22 00:46:52 +0700

    latest update R file

---
+++ Part 1 - Data Preprocessing/data_preprocessing.R
@@ -0,0 +1,27 @@
+#Import dataset
+dataset = read.csv('Data.csv')
+
+#Handling missing values
+dataset$Age = ifelse(is.na(dataset$Age),
+                     ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
+                     dataset$Age)
+
+dataset$Salary = ifelse(is.na(dataset$Salary),
+                        ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
+                        dataset$Salary)
+
+#Categorical values
+dataset$Country = factor(dataset$Country,
+                         levels = c('France', 'Spain', 'Germany'),
+                         labels = c(1,2,3))
+
+dataset$Purchased = factor(dataset$Purchased,
+                           levels = c('Yes', 'No'),
+                           labels = c(1,0))
+
+#Train test split
+library(caTools)
+set.seed(9873)
+split = sample.split(dataset$Purchased, 0.8)
+training_set = subset(dataset, split == TRUE)
+test_set = subset(dataset, split == FALSE)
