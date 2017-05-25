49272ba0faf80fc6031cdb0eff5de3297fa837d0
Mursil Shadruddin Afkar <afkarshad@gmail.com>
2017-05-26 01:59:40 +0700

    add code to feature scaling in python and R

--- Part 1 - Data Preprocessing/data_preprocessing.R
+++ Part 1 - Data Preprocessing/data_preprocessing.R
@@ -1,7 +1,7 @@
-#Import dataset
+# Import dataset
 dataset = read.csv('Data.csv')
 
-#Handling missing values
+# Handling missing values
 dataset$Age = ifelse(is.na(dataset$Age),
                      ave(dataset$Age, FUN = function(x) mean(x, na.rm = TRUE)),
                      dataset$Age)
@@ -10,7 +10,7 @@ dataset$Salary = ifelse(is.na(dataset$Salary),
                         ave(dataset$Salary, FUN = function(x) mean(x, na.rm = TRUE)),
                         dataset$Salary)
 
-#Categorical values
+# Categorical values
 dataset$Country = factor(dataset$Country,
                          levels = c('France', 'Spain', 'Germany'),
                          labels = c(1,2,3))
@@ -19,9 +19,13 @@ dataset$Purchased = factor(dataset$Purchased,
                            levels = c('Yes', 'No'),
                            labels = c(1,0))
 
-#Train test split
+# Train test split
 library(caTools)
 set.seed(9873)
 split = sample.split(dataset$Purchased, 0.8)
 training_set = subset(dataset, split == TRUE)
 test_set = subset(dataset, split == FALSE)
+
+# Feature scaling
+training_set[, 2:3] = scale(training_set[, 2:3])
+test_set[, 2:3] = scale(test_set[, 2:3])
--- Part 1 - Data Preprocessing/data_preprocessing.py
+++ Part 1 - Data Preprocessing/data_preprocessing.py
@@ -5,23 +5,23 @@ Created on Tue May 16 23:52:00 2017
 @author: Afkar
 """
 
-#Importing libraries
+# Importing libraries
 import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-#Importing dataset
+# Importing dataset
 dataset = pd.read_csv('Data.csv')
 X = dataset.iloc[:, :-1].values
 y = dataset.iloc[:, 3].values
 
-#Handling missing values
+# Handling missing values
 from sklearn.preprocessing import Imputer
 imputer = Imputer(missing_values='NaN', axis=0)
 imputer = imputer.fit(X[:, 1:3])
 X[:, 1:3] = imputer.transform(X[:, 1:3])
 
-#Encoding categorical data
+# Encoding categorical data
 from sklearn.preprocessing import LabelEncoder, OneHotEncoder
 encoder_X = LabelEncoder()
 X[:, 0] = encoder_X.fit_transform(X[:, 0])
@@ -30,6 +30,27 @@ X = hotencoder.fit_transform(X).toarray()
 encoder_y = LabelEncoder()
 y = encoder_y.fit_transform(y)
 
-#Split training and test set
+# Split training and test set
 from sklearn.model_selection import train_test_split
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.2, random_state = 9873)
+
+# Feature scaling
+from sklearn.preprocessing import StandardScaler
+sc_X = StandardScaler()
+X_train = sc_X.fit_transform(X_train)
+X_test = sc_X.transform(X_test)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
